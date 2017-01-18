9785f4b26f021bf129ce1a346f1b966c67094752
chrassy <aljaz.klanecek@gmail.com>
2017-01-18 22:53:16 +0100

    added AJAX and some content
    Added AJAX to prevent BG flashes on load, also content added on several
    pages

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//AJAX za context
+
+$(function(){
+  $(".classloader").click(function(){
+  	if ($(this).attr('id') == "pregled") {
+    	$(".spremembaVsebine").load("pregled.html");
+     }
+  	else if ($(this).attr('id') == "visum") {
+    	$(".spremembaVsebine").load("visum.html");
+     }
+     else if ($(this).attr('id') == "trafficData") {
+     	$(".spremembaVsebine").load("trafficData.html");
+     }     
+  });
+});
+
+
+
+
+//Obarvan trenutni podmeni
+
+$(function() {  
+$('.clicked').click(function(){
+	$('.clicked').css('color', '#4B4A4D');
+    $(this).css('color', '#E02129');
+
+  });
+});
+
+
+//alway keep one sidebar open
+
