12bb5079fa0b0d4a9809709305b689c6363b2598
chrassy <aljaz.klanecek@gmail.com>
2017-02-18 22:37:15 +0100

    added mainPage links to ajax content and submit form for demo version

--- js/script.js
+++ js/script.js
@@ -26,15 +26,11 @@ $('.clicked').on('click',function(e){
 });
 
 //AJAX za context
-$(function(){
+$(function(){  
   $(".classloader").click(function(){
   	 if ($(this).attr('id') == "pregled") {
     	$(".spremembaVsebine").load("pregled.html");    	
-     }
-  	 else if ($(this).attr('id') == "visum2") {
-      $(".body").load("proizvodi.html");
-    	$(".spremembaVsebine").load("visum.html");    	
-     }
+     }  	 
      else if ($(this).attr('id') == "visum") {
       $(".spremembaVsebine").load("visum.html");      
      }
@@ -49,11 +45,42 @@ $(function(){
      }
      else if ($(this).attr('id') == "podrzka") {
       $(".spremembaVsebine").load("podrzka.html");      
-     }            
+     }
+     else if ($(this).attr('id') == "viswalk") {
+      $(".spremembaVsebine").load("viswalk.html");      
+     }                  
   });
 });
 
 
+
+//mainPage links to ajax from pregled 
+$(function(){
+      
+       $("#pregled2").click();     
+       $("#pregled").click();
+       $("#pregled").css('color', "#E02129");
+       $("#visum").css('color', "#4B4A4D"); 
+
+       $("#visum2").click();     
+       $("#visum").click();
+       $("#visum").css('color', "#E02129");
+       $("#pregled").css('color', "#4B4A4D"); 
+
+       $("#trening2").click();     
+       $("#trening").click();
+       $("#trening").css('color', "#E02129");
+       $("#podrzka").css('color', "#4B4A4D"); 
+
+       $("#podrzka2").click();     
+       $("#podrzka").click();
+       $("#podrzka").css('color', "#E02129");
+       $("#trening").css('color', "#4B4A4D"); 
+
+});
+     
+    
+    
 //Obarvan trenutni podmeni in meni
 $(function() {  
 $('.clicked').click(function(){
